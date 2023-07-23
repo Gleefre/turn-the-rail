2b1c5b9a0e6054ead82e2cf47ac76e425aa30de3
Gleefre <varedif.a.s@gmail.com>
2023-07-23 23:33:59 +0200

    More drawing

--- src/draw.lisp
+++ src/draw.lisp
@@ -19,55 +19,28 @@
         (draw-coins)
         (draw-life-orbs)
         (draw-train))
-      (s+:with-fit (16 12 400 300)
-       (s:with-pen (s:make-pen :fill (s:hex-to-color "#83769C") :weight 0)
-         (s:polygon -1 -1
-                    -1  3
-                     2  0
-                     6  0
-                     8  2
-                     8  5
-                    12  5
-                    11  2
-                    15  2
-                    14  5
-                    15  5
-                    15  6
-                    17  6
-                    17 -1)
-         (s:polygon -1 13
-                    -1 10
-                     1 12
-                     2 12
-                     3 11
-                     4 12
-                     5 12
-                     6 11
-                     9 11
-                    10 12
-                    11 12
-                    12 11
-                    13 12
-                    14 12
-                    15 11
-                    15 10
-                    17 10
-                    17 13))))))
+      (s+:with-color ((s:hex-to-color "#83769C"))
+        (draw-train-outline 400 300)))))
 
 (defun draw-background ()
-  (s:background (s:hex-to-color "#AB5236")))
+  (s:background (s:hex-to-color "#008751")))
+
+(defparameter +rail-length+ 25)
 
 (defun draw-rail ()
   (s+:with-translate ((- (cx *game*)) (- (cy *game*)))
     (s+:with-translate ((x *game*) (y *game*))
       (s+:with-rotate ((α *game*))
-        (s:line -500 0 500 0)
-        (sc:with-freeze *game-clock*
-          (loop for x from -100 to 100
-                do (s+:with-translate ((* 60 x) 0)
-                     (s+:with-translate ((- (mod (* +train-speed+ (sc:time *game-clock*)) 60)) 0)
-                       (s+:with-color (s:+magenta+ :stroke)
-                         (s:line 0 0 20 0))))))))))
+        (let ((stage (mod (* +train-speed+ (sc:time *game-clock*)) +rail-length+)))
+          (s+:with-color ((s:hex-to-color "#AB5236"))
+            (s+:with-translate ((- stage) 0)
+              (loop for x from -100 to 100
+                    do (s+:with-translate ((* +rail-length+ x) 0)
+                         (s:rect -2 10 4 10))))))
+        (s+:with-color ((s:hex-to-color "#C2C3C7"))
+          (s:rect -200 13 800 4))
+        #+nil
+        (s:line -500 0 500 0)))))
 
 (defun draw-rocks ()
   (s+:with-translate ((- (cx *game*)) (- (cy *game*)))
@@ -95,11 +68,19 @@
                  (s+:with-translate (0 -10/12)
                    (draw-train-shape 40/3 10)))))))
 
+(defun draw-train ()
+  (s+:with-translate ((- (cx *game*)) (- (cy *game*)))
+    (s+:with-translate ((x *game*) (y *game*))
+      (s+:with-rotate ((α *game*))
+        (s+:with-scale (1 (+ 1 (/ (sin (cycle-pos *game-clock* -30 30 :multiplier 2))
+                                  10)))
+          (s+:with-color ((s:hex-to-color "#7E2553"))
+            (draw-train-shape 40 30)))))))
+
 (defun draw-train-shape (w h)
   (s+:with-fit (16 12 w h 8 6)
-    (s:polygon 0 2 2 0 6 0 8 2 8 5 12 5 11 2 15 2 14 5 15 5 15 6 16 6
-               16 10 15 10 15 11 14 12 13 12 12 11 11 12 10 12 9 11 6 11 5 12
-               4 12 3 11 2 12 1 12 0 11)))
+    (s:polygon 0 2 2 0 6 0 8 2 8 5 12 5 11 2 15 2 14 5 15 5 15 6 16 6 16 10 15 10 15 11
+               14 12 13 12 12 11 11 12 10 12 9 11 6 11 5 12 4 12 3 11 2 12 1 12 0 11)))
 
 (defun draw-heart-shape (w h)
   (s+:with-fit (6 6 w h 3 3)
@@ -109,14 +90,12 @@
   (s+:with-fit (4 4 w h 2 2)
     (s:polygon 0 1 1 0 3 0 4 1 4 3 3 4 1 4 0 3)))
 
-(defun draw-train ()
-  (s+:with-translate ((- (cx *game*)) (- (cy *game*)))
-    (s+:with-translate ((x *game*) (y *game*))
-      (s+:with-rotate ((α *game*))
-        (s+:with-scale (1 (+ 1 (/ (sin (cycle-pos *game-clock* -30 30 :multiplier 2))
-                                  10)))
-          (s+:with-color ((s:hex-to-color "#7E2553"))
-            (draw-train-shape 40 30)))))))
+(defun draw-train-outline (w h)
+  (s+:with-fit (16 12 w h)
+    (s+:with-scissor (0 0 16 12)
+      (s:polygon -1 -1 -1 3 2 0 6 0 8 2 8 5 12 5 11 2 15 2 14 5 15 5 15 6 17 6 17 -1)
+      (s:polygon -1 13 -1 10 1 12 2 12 3 11 4 12 5 12 6 11 9 11 10
+                 12 11 12 12 11 13 12 14 12 15 11 15 10 17 10 17 13))))
 
 ;; Header
 
