2abe4c4be41ff3cffea90452efb8be64a13f1706
Gleefre <varedif.a.s@gmail.com>
2023-07-23 21:32:31 +0200

    Add some keys

--- src/game.lisp
+++ src/game.lisp
@@ -24,3 +24,17 @@
         (*game-clock* (game-window-clock window))
         (*game-window* window))
     (call-next-method)))
+
+(defmethod kit.sdl2:keyboard-event ((window game-window) state ts rep? keysym)
+  (unless rep?
+    (case (sdl2:scancode keysym)
+      (:scancode-space
+       (case state
+         (:keyup (release-space))
+         (:keydown (press-space))))
+      (:scancode-m
+       (when (eq state :keydown)
+         (click-on-mute-soundtrack)))
+      (:scancode-f
+       (when (eq state :keydown)
+         (click-on-mute-sfx))))))
